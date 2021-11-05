3a34e5c104cae76e91a57c19fff208788e3ec990
Sharshenov Islam <islambulut@mail.ru>
2021-11-05 18:58:29 +0600

    edding description

--- bot.py
+++ bot.py
@@ -15,21 +15,26 @@ def start(message):
     text = "Здравствуйте! \nДобро пожаловать в наш телеграмм бот! \n Наш бот будет отправлять вам все акции известных кафе города Бишкек \nВыберите свои дальнейшие действия!"
     bot.send_message(message.chat.id, text, reply_markup=markup)
 
+
 @bot.message_handler(content_types=['text'])
 def echo(message):
     get_message = message.text
     if get_message == 'Dodo Pizza':
         sales_list = get_data()
-        markup = types.InlineKeyboardMarkup(row_width = 4)
         for sale in sales_list:
-            bot.send_message(message.chat.id, sale['title'])
-            item = types.InlineKeyboardButton
-            
+            markup = types.InlineKeyboardMarkup(row_width = 1)
+            items = types.InlineKeyboardButton('Описание', callback_data=str(sale['number']))
+            markup.add(items)
+            bot.send_message(message.chat.id, sale['title'],  reply_markup=markup)
 
-@bot.callback_query_handler(func = lambda call: True)
+@bot.callback_query_handler(func =lambda call: True)
 def callback_inline(call):
-    if call.message
-
+    if call.message:
+        sales_list = get_data()
+        for sale in sales_list:
+            if call.data == str(sale['number']):
+                text = sale['description']
+                bot.edit_message_text(chat_id=call.message.chat.id,text = text, message_id=call.message.message_id, reply_markup=None)
 
 
 
